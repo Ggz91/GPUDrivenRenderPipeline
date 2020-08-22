458525020e1753c5b25c351ea20e108d4e6ac8c5
529038378 <529038378@qq.com>
2020-08-22 21:32:05 +0800

    fill G-Buffer pass

---
+++ Shaders/Default.hlsl
@@ -0,0 +1,142 @@
+#include "Common.hlsl"
+
+struct VertexIn
+{
+	float3 PosL    : POSITION;
+    float3 NormalL : NORMAL;
+	float2 TexC    : TEXCOORD;
+	float3 TangentL : TANGENT;
+};
+
+struct VertexOut
+{
+	float4 PosH    : SV_POSITION;
+    float3 PosW    : POSITION2;
+    float3 NormalW : NORMAL;
+	float3 TangentW : TANGENT;
+	float2 TexC    : TEXCOORD;
+};
+
+struct DeferredVertexOut
+{
+	float4 PosH    : SV_POSITION;
+    float3 NormalW : NORMAL;
+	float2 TexC    : TEXCOORD;
+};
+
+struct DeferredPixelOut
+{
+   float4 Normal_UV: SV_Target0;
+   uint Mat_ID: SV_Target1;
+};
+
+VertexOut VS(VertexIn vin)
+{
+	VertexOut vout = (VertexOut)0.0f;
+
+	// Fetch the material data.
+	MaterialData matData = gMaterialData[gMaterialIndex];
+	
+    // Transform to world space.
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+    vout.PosW = posW.xyz;
+
+    // Assumes nonuniform scaling; otherwise, need to use inverse-transpose of world matrix.
+    vout.NormalW = mul(vin.NormalL, (float3x3)gWorld);
+	
+	vout.TangentW = mul(vin.TangentL, (float3x3)gWorld);
+
+    // Transform to homogeneous clip space.
+    vout.PosH = mul(posW, gViewProj);
+
+   
+	// Output vertex attributes for interpolation across triangle.
+	//float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
+	//vout.TexC = mul(texC, matData.MatTransform).xy;
+	vout.TexC = vin.TexC;
+
+    return vout;
+}
+
+float4 PS(VertexOut pin) : SV_Target
+{
+    // Fetch the material data.
+	MaterialData matData = gMaterialData[gMaterialIndex];
+	float4 diffuseAlbedo = matData.DiffuseAlbedo;
+	float3 fresnelR0 = matData.FresnelR0;
+	float  roughness = matData.Roughness;
+	uint diffuseMapIndex = matData.DiffuseMapIndex;
+	uint normalMapIndex = matData.NormalMapIndex;
+
+    // Dynamically look up the texture in the array.
+    diffuseAlbedo = gTextureMaps[diffuseMapIndex].Sample(gsamAnisotropicWrap, pin.TexC);
+
+	// Interpolating normal can unnormalize it, so renormalize it.
+    pin.NormalW = normalize(pin.NormalW);
+	
+    float4 normalMapSample = gTextureMaps[normalMapIndex].Sample(gsamAnisotropicWrap, pin.TexC);
+	float3 bumpedNormalW = NormalSampleToWorldSpace(normalMapSample.rgb, pin.NormalW, pin.TangentW);
+
+	// Uncomment to turn off normal mapping.
+    //bumpedNormalW = pin.NormalW;
+
+    // Vector from point being lit to eye. 
+    float3 toEyeW = normalize(gEyePosW - pin.PosW);
+
+    // Light terms.
+    float4 ambient = gAmbientLight*diffuseAlbedo;
+
+    // Only the first light casts a shadow.
+    float3 shadowFactor = float3(1.0f, 1.0f, 1.0f);
+
+    const float shininess = (1.0f - roughness) * normalMapSample.a;
+    Material mat = { diffuseAlbedo, fresnelR0, shininess };
+    float4 directLight = ComputeLighting(gLights, mat, pin.PosW,
+        bumpedNormalW, toEyeW, shadowFactor);
+
+    float4 litColor = ambient + directLight;
+
+	// Add in specular reflections.
+    float3 r = reflect(-toEyeW, bumpedNormalW);
+    float4 reflectionColor = float4(1.0, 1.0, 1.0, 1);
+    float3 fresnelFactor = SchlickFresnel(fresnelR0, bumpedNormalW, r);
+    litColor.rgb += shininess * fresnelFactor * reflectionColor.rgb;
+	
+    // Common convention to take alpha from diffuse albedo.
+    litColor.a = diffuseAlbedo.a;
+    return litColor;
+}
+
+DeferredVertexOut DeferredVS(VertexIn vin)
+{
+	DeferredVertexOut vout = (DeferredVertexOut)0.0f;
+
+	// Fetch the material data.
+    // Transform to world space.
+    float4 posW = mul(float4(vin.PosL, 1.0f), gWorld);
+
+    // Assumes nonuniform scaling; otherwise, need to use inverse-transpose of world matrix.
+    vout.NormalW = mul(vin.NormalL, (float3x3)gWorld);
+    // Transform to homogeneous clip space.
+    vout.PosH = mul(posW, gViewProj);
+
+	// Output vertex attributes for interpolation across triangle.
+	//float4 texC = mul(float4(vin.TexC, 0.0f, 1.0f), gTexTransform);
+	//vout.TexC = mul(texC, matData.MatTransform).xy;
+	vout.TexC = vin.TexC;
+
+    return vout;
+}
+
+DeferredPixelOut DeferredPS(DeferredVertexOut pin)
+{
+    DeferredPixelOut res;
+    res.Normal_UV.x = pin.NormalW.x;
+    res.Normal_UV.y = pin.NormalW.y;
+    res.Normal_UV.z = pin.TexC.x;
+    res.Normal_UV.a = pin.TexC.y;
+    res.Mat_ID = gMaterialIndex;
+    return res;
+}
+
+
